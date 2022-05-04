6d5e077a13196d06b420817c8e31e18d7a74f42f
thanhcontentstack <thanh.do@contentstack.com>
2022-05-04 09:05:01 -0700

    fix dev scripts for package.json

--- ui/src/containers/PageAnalysisDashboardWidget/index.tsx
+++ ui/src/containers/PageAnalysisDashboardWidget/index.tsx
@@ -2,12 +2,17 @@
 import React, { useEffect, useState } from "react";
 /* Import other node modules */
 import ContentstackAppSdk from "@contentstack/app-sdk";
-import { FieldLabel } from "@contentstack/venus-components";
+import { TextInput, Button, FieldLabel, InstructionText } from "@contentstack/venus-components"
 import { TypeSDKData } from "../../common/types";
 /* Import our modules */
 /* Import node module CSS */
 /* Import our CSS */
 import "./styles.scss";
+
+interface IDomainSettings {
+  domainName: string,
+  results: any
+}
 
 const DashboardWidget: React.FC = function () {
   const [state, setState] = useState<TypeSDKData>({
@@ -16,11 +21,58 @@ const DashboardWidget: React.FC = function () {
     appSdkInitialized: false,
   });
 
+  const [domainSettings, setDomainSettings] = useState<IDomainSettings>({
+    domainName: '',
+    results: null
+  });
+
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  const analyzeWebsite = async (domainName: string) => {
+    let key = 'AIzaSyCfraf3NrUxoHUygKGunRvRefWq0zXw_wY';
+    let response = await fetch(`https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${domainName}&key=${key}`)
+
+    if (!response.ok) {
+      throw new Error("HTTP status " + response.status);
+    }
+    let toJSON = await response.json()
+    
+    setIsLoading(false)
+    setDomainSettings({
+      domainName: domainSettings.domainName,
+      results: toJSON
+    })
+
+    setTimeout(() => {
+      console.log(domainSettings.results)
+    }, 5000);
+  }
+
+  const handleInputField = (event:any) => {
+    // console.log(event.target.value)
+    setDomainSettings({
+      domainName: event.target.value,
+      results: {}
+    })
+    // console.log(domainSettings)
+  }
+
+  const onSubmit = () => {
+    if (domainSettings.domainName) {
+      setIsLoading(true)
+      analyzeWebsite(domainSettings.domainName);
+    }
+  }
+
   useEffect(() => {
+    console.log(domainSettings.results)
+
     ContentstackAppSdk.init().then(async (appSdk) => {
       const config = await appSdk.getConfig();
-      appSdk?.location?.DashboardWidget?.frame?.enableAutoResizing?.();
+      appSdk?.location?.DashboardWidget?.frame?.enableResizing?.();
       appSdk?.location?.DashboardWidget?.frame?.updateHeight?.(800);
+      appSdk?.location?.DashboardWidget?.frame?.enableAutoResizing?.();
+
       setState({
         config,
         location: appSdk.location,
@@ -30,20 +82,48 @@ const DashboardWidget: React.FC = function () {
   }, []);
 
   return (
-    <div className="layout-container">
-      <h2>Dashboard widget</h2>
-      {state.appSdkInitialized && (
-        // <>
-        //   Your dashboard UI must be developed here based on the state variable
-        //   {`Your current state is ${JSON.stringify(state)}`}
-        // </>
-        <FieldLabel
-          htmlFor={state?.config?.configField1}
-          className="Dashboard-field"
-        >
-          {state.config.configField1}
-        </FieldLabel>
-      )}
+    <div className="container">
+      {
+      state.appSdkInitialized
+      ?
+      <>
+        <div className="control-wrapper">
+          <InstructionText>Enter a web page URL</InstructionText>
+          <TextInput
+            type='text'
+            autoFocus={true}
+            width='medium'
+            value={domainSettings.domainName}
+            placeholder='Enter a web page URL'
+            onChange={(e:any) => handleInputField(e)}
+          />
+          <Button 
+            size='large' 
+            isLoading={isLoading} 
+            onClick={onSubmit}>Analyze</Button>
+        </div>
+        {isLoading ? <div className="loading-message">Running performance analysis</div> : null}
+
+
+        {domainSettings.results ?
+        
+        <div className="analysis-results">
+          <p>First Contentful Paint: {domainSettings?.results?.loadingExperience?.metrics?.FIRST_CONTENTFUL_PAINT_MS.category}</p>
+          <p>First Contentful Paint: {domainSettings?.results?.lighthouseResult?.audits['first-contentful-paint'].displayValue}</p>
+          <p>Speed Index: {domainSettings?.results?.lighthouseResult?.audits['speed-index'].displayValue}</p>
+          
+          
+          </div>
+        : null
+      
+      
+      }
+
+
+      </>
+      :
+      null 
+      }
     </div>
   );
 };
