df172a9ca7b9873fe651011b121e3a95cae6d8f4
Jaime Santos Alcon <jaime.santosalcon@contentstack.com>
2022-05-03 20:19:57 -0400

    had to commit this before I created my  branch

--- ui/src/containers/SentimentAnalysisWidget/index.tsx
+++ ui/src/containers/SentimentAnalysisWidget/index.tsx
@@ -1,14 +1,16 @@
-/* Import React modules */
-import React, { useEffect, useState } from "react";
-/* Import other node modules */
-import ContentstackAppSdk from "@contentstack/app-sdk";
-import { FieldLabel } from "@contentstack/venus-components";
-import { TypeSDKData, TypeEntryData } from "../../common/types";
-/* Import our modules */
-import localeTexts from "../../common/locale/en-us";
 /* Import node module CSS */
 /* Import our CSS */
 import "./styles.scss";
+
+/* Import React modules */
+import React, { useEffect, useState } from "react";
+import { TypeEntryData, TypeSDKData } from "../../common/types";
+
+/* Import other node modules */
+import ContentstackAppSdk from "@contentstack/app-sdk";
+import { FieldLabel } from "@contentstack/venus-components";
+/* Import our modules */
+import localeTexts from "../../common/locale/en-us";
 
 const SidebarWidget: React.FC = function () {
   const [entryData, setEntryData] = useState<TypeEntryData>({ title: "" });
@@ -19,46 +21,49 @@ const SidebarWidget: React.FC = function () {
   });
 
   useEffect(() => {
-    ContentstackAppSdk.init().then(async (appSdk) => {
-      const config = await appSdk?.getConfig();
-
-      const entryDataFromSDK =
-        appSdk?.location?.SidebarWidget?.entry?.getData();
-      setEntryData(entryDataFromSDK); // entryData is the whole entry object from CMS that contains all the data in the current entry for which sidebar is opened.
+    // eslint-disable-next-line no-restricted-globals
+    if (self === top) {
+      const msg = "This extension can only be used in Contentstack";
+      console.log("Extension loaded outside Contentstack!", msg);
       setState({
-        config,
-        location: appSdk.location,
-        appSdkInitialized: true,
+        config: {},
+        location: {},
+        appSdkInitialized: false,
       });
-    });
+    } else {
+      ContentstackAppSdk.init().then(async (appSdk) => {
+        const config = await appSdk?.getConfig();
+
+        const entryDataFromSDK = appSdk?.location?.SidebarWidget?.entry?.getData();
+        setEntryData(entryDataFromSDK); // entryData is the whole entry object from CMS that contains all the data in the current entry for which sidebar is opened.
+        setState({
+          config,
+          location: appSdk.location,
+          appSdkInitialized: true,
+        });
+      });
+    }
   }, []);
 
   return (
-    <div className="layout-container">
-      {state.appSdkInitialized && (
-        // <>
-        // Your sidebar UI must be developed here based on the state variable
-        // {`Your current state is ${JSON.stringify(state)}`}
-        // {`Your current entryData is ${JSON.stringify(entryData)}`}
-        // </>
-        <div className="sidebar-wrapper">
-          <FieldLabel
-            htmlFor={state?.config?.configField1}
-            className="sidebar-field"
-          >
-            {state.config.configField1}
-          </FieldLabel>
-          <div className="entry-wrapper">
-            <FieldLabel htmlFor="entry-title" className="sidebar-field">
-              {localeTexts.sidebarWidget.titleCaption}
-            </FieldLabel>
-            <FieldLabel htmlFor={entryData?.title} className="sidebar-field">
-              {entryData?.title}
-            </FieldLabel>
-          </div>
-        </div>
-      )}
-    </div>
+    // <div className="layout-container">
+    //   {state.appSdkInitialized && (
+    //     <div className="sidebar-wrapper">
+    //       <FieldLabel htmlFor={state?.config?.configField1} className="sidebar-field">
+    //         {state.config.configField1}
+    //       </FieldLabel>
+    //       <div className="entry-wrapper">
+    //         <FieldLabel htmlFor="entry-title" className="sidebar-field">
+    //           {localeTexts.sidebarWidget.titleCaption}
+    //         </FieldLabel>
+    //         <FieldLabel htmlFor={entryData?.title} className="sidebar-field">
+    //           {entryData?.title}
+    //         </FieldLabel>
+    //       </div>
+    //     </div>
+    //   )}
+    // </div>
+    <div className="layout-container"></div>
   );
 };
 
